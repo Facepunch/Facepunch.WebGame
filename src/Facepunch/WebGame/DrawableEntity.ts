cf9e04d4a2f6a23b748e29cde7b3288b154967b6
James King <james.king@facepunchstudios.com>
2017-03-23 16:20:07 +0000

    Added DrawableEntity

---
+++ src/Facepunch/WebGame/DrawableEntity.ts
@@ -0,0 +1,36 @@
+/// <reference path="Entity.ts"/>
+
+namespace Facepunch {
+    export namespace WebGame {
+        export class DrawableEntity extends Entity implements IDrawListItem {
+            protected readonly drawable = new DrawListItem();
+
+            constructor(isStatic: boolean = false) {
+                super();
+
+                this.drawable.entity = this;
+                this.drawable.isStatic = isStatic;
+            }
+
+            getIsVisible(): boolean {
+                return this.drawable.getIsVisible();
+            }
+
+            getIsInDrawList(drawList: DrawList): boolean {
+                return this.drawable.getIsInDrawList(drawList);
+            }
+
+            onAddToDrawList(list: DrawList): void {
+                this.drawable.onAddToDrawList(list);
+            }
+
+            onRemoveFromDrawList(list: DrawList): void {
+                this.drawable.onRemoveFromDrawList(list);
+            }
+
+            getMeshHandles(): MeshHandle[] {
+                return this.drawable.getMeshHandles();
+            }
+        }
+    }
+}
--- src/Facepunch/WebGame/StaticProp.ts
+++ src/Facepunch/WebGame/StaticProp.ts
@@ -1,17 +1,13 @@
+/// <reference path="DrawableEntity.ts"/>
 
 namespace Facepunch {
     export namespace WebGame {
-        export class StaticProp extends Entity implements IDrawListItem {
-            private readonly drawable = new DrawListItem();
-
+        export class StaticProp extends DrawableEntity {
             private model: Model;
             private tint: Vector3;
 
             constructor() {
-                super();
-
-                this.drawable.entity = this;
-                this.drawable.isStatic = true;
+                super(true);
             }
 
             setColorTint(color: IVector3): void {
@@ -51,26 +47,6 @@ namespace Facepunch {
             
             getModel(): Model {
                 return this.model;
-            }
-
-            getIsVisible(): boolean {
-                return this.drawable.getIsVisible();
-            }
-
-            getIsInDrawList(drawList: DrawList): boolean {
-                return this.drawable.getIsInDrawList(drawList);
-            }
-
-            onAddToDrawList(list: DrawList): void {
-                this.drawable.onAddToDrawList(list);
-            }
-
-            onRemoveFromDrawList(list: DrawList): void {
-                this.drawable.onRemoveFromDrawList(list);
-            }
-
-            getMeshHandles(): MeshHandle[] {
-                return this.drawable.getMeshHandles();
             }
         }
     }
